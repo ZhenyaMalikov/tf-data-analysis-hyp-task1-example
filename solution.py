0c3665719eca64983f00e0227d89b3ca5cb7871f
ZhenyaMalikov <130054976+ZhenyaMalikov@users.noreply.github.com>
2023-04-13 17:58:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.proportion import proportions_ztest
+from scipy.stats import norm
 
 chat_id = 516575251 # Ваш chat ID, не меняйте название переменной
 
@@ -9,7 +9,15 @@ def solution(x_success: int,
              y_success: int, 
              y_cnt: int) -> bool:
     alpha = 0.09
-    successes = np.array([x_success, y_success])
-    alls = np.array([x_cnt, y_cnt])
-    p_val = proportions_ztest(successes, alls, alternative='larger')[1]
-    return True if p_val < alpha else False
+    control_conversion = x_success/x_cnt
+    test_conversion = y_success/y_cnt
+    #находим общую конверсию
+    total_conversion = (x_success + y_success)/(x_cnt + y_cnt)
+    #вычисляем стандартную ошибку
+    se = (total_conversion * (1 - total_conversion) * (1/x_cnt + 1/y_cnt))
+    #находим z-статистику
+    z = (control_conversion - test_conversion)/se
+    #находим критическое значение z-статистики
+    critical_value = norm.ppf(1 - alpha/2)
+    #отклоняем нулевую гипотезу, если pval < уровня значимости (то есть pval < alpha => True)
+    return abs(z) > critical_value
